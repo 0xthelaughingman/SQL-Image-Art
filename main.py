7f874007d51af88db46197509d3294762b0ce1a5
Chirag Soni <csoni.chirag@gmail.com>
2023-10-09 13:05:46 +0530

    comments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 
 def get_tuples(img):
 
+    # convert the input image to grayscale->black & white + a flip
     grayImage = cv2.flip(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 0)
     (thresh, img) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
 
@@ -12,27 +13,33 @@ def get_tuples(img):
     row, col = img.shape
     max_tuples = 0
 
+    # Iterate through the Image columns, goal is to get the maximum segments
     for j in range(col):
         local_max = 1
         for i in range(row):
+            # value at current element different from previous -> new segment
             if i != 0 and img[i][j] != img[i - 1][j]:
                 local_max += 1
 
         if local_max > max_tuples:
             max_tuples = local_max
 
+    # Init the tuple array with default 0s.
     output_tuples = []
     for i in range(max_tuples):
         output_tuples.append([0] * col)
 
+    # Set the exact values for each segment
     for j in range(col):
         tupple_index = 0
         cur_val = 0
         for i in range(row):
+            # same segment, continue
             if i == 0 or img[i][j] == img[i - 1][j]:
                 cur_val += 1
                 output_tuples[tupple_index][j] = cur_val
 
+            # new segment, reset values, increment index
             if i != 0 and img[i][j] != img[i - 1][j]:
                 cur_val = 1
                 tupple_index += 1
@@ -83,7 +90,7 @@ def setSQL(tuples):
         cur_string = "\nLEFT JOIN row" + str(i) + " ON row0.index = row" + str(i) + ".index"
         rows_post = rows_post + cur_string
 
-    # concat all
+    # concat all, single string output
     return base + rows + rows_pre + rows_post
 
 
